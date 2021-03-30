abfa76b960a082de3daef99223f4bbbbf6540f83
Paul O'Brien <65104517+PVOBrien@users.noreply.github.com>
2021-03-30 09:16:08 -0700

    remove curly brackets from isLoading...
    why the heck were they there to begin with?

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     return (
       <>
         <Router>
-          {<IsLoadingAndError>}
+          <IsLoadingAndError>
           <Header />
           <Switch>
             <Route exact path="/">
@@ -31,7 +31,7 @@ class App extends React.Component {
             </Route>
           </Switch>
           <Footer />
-          {</IsLoadingAndError>}
+          </IsLoadingAndError>
         </Router>
       </>
     )
